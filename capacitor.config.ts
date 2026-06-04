d06071a7990b82585ec4b099239bbd2a913da5d0
RenanCafe <renan.cafe@sp.senai.br>
2026-06-03 23:01:23 -0300

    feat: add capacitor android build support

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.senai.jogoxadrez',
+  appName: ' Jogo de Xadrez',
+  webDir: 'out',
+};
+
+export default config;
